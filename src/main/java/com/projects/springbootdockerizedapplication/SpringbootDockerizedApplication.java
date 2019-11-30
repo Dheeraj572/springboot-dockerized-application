8e6022236a25acd2c9a5e9c4ec8c92afcfb2f57a
Dheeraj <kontham.dheeraj@gmail.com>
2019-11-30 16:25:01 +0530

    initial commit to create remote repository

---
+++ src/main/java/com/projects/springbootdockerizedapplication/SpringbootDockerizedApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springbootdockerizedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/springbootdockerizedapplication/SpringbootDockerizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.springbootdockerizedapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
