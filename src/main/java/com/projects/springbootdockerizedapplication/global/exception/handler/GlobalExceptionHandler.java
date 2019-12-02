43626b6aa8998290ee08c3db8725971e9b93923b
Dheeraj <kontham.dheeraj@gmail.com>
2019-12-02 13:12:04 +0530

    Updated GlobalExceptionHandler class annotationt to RestControllerAdvice

--- src/main/java/com/projects/springbootdockerizedapplication/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/projects/springbootdockerizedapplication/global/exception/handler/GlobalExceptionHandler.java
@@ -5,13 +5,14 @@ import org.slf4j.MDC;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import com.projects.springbootdockerizedapplication.exception.StudentException;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(Exception.class)
--- src/main/java/com/projects/springbootdockerizedapplication/service/StudentService.java
+++ src/main/java/com/projects/springbootdockerizedapplication/service/StudentService.java
@@ -20,11 +20,14 @@ public class StudentService implements IStudentService{
 	private static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
 	
 	List<Student> studentList = Arrays.asList(new Student(1L, "Student1", "Java"),new Student(2L, "Student2", "C"),new Student(3L, "Student3", "Python"));
+	
 	public List<StudentResponse> getStudents(){
 		
 		LOGGER.info("Mapping Student list to StudentResponse List --------");
+		
 		List<StudentResponse> studentResponseList = studentList.stream()
 				.map(mapper -> objectMapper.convertValue(mapper, StudentResponse.class)).collect(Collectors.toList());
+		
 		LOGGER.info("Mapped Student list to StudentResponse List --------");
 		return studentResponseList;
 	}
